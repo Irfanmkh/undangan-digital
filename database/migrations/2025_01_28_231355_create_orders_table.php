34b64d75d01bebb61b9beb12983ba97b24b86292
irfanmkh <irfankhakiki17@gmail.com>
2025-01-29 21:46:42 +0700

    first commit

---
+++ database/migrations/2025_01_28_231355_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('template_id')->constrained();
+            $table->string('nama_customer');
+            $table->string('email_customer');
+            $table->decimal('total_harga', 10, 2);
+            $table->string('status')->default('pending');
+            $table->string('link_pembayaran')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
