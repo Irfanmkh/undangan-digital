e547a4d982fee8d20c3f8c6a39b513e08366b5e2
Irfan Maulana Khakiki <irfankhakiki17@gmail.com>
2025-01-30 16:40:55 +0700

    update model

--- app/Models/order.php
+++ app/Models/order.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class order extends Model
 {
     //
+    protected $fillable = [
+        "nama_customer",
+        "email_customer",
+        "template_id",
+        "total_harga",
+        'link_pembayaran',
+    ];
+
+    public function template(){
+
+        return $this->belongsTo(template::class);
+    }
 }
--- app/Models/template.php
+++ app/Models/template.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class template extends Model
 {
     //
+    protected $fillable = [
+        "nama",
+        "deskripsi",
+        "harga",
+    ];
 }
