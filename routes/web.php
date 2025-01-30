0c605efff06b769e34fdd900aa22f210c62d454e
Irfan Maulana Khakiki <irfankhakiki17@gmail.com>
2025-01-30 16:38:52 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/checkout/{template}', [CheckoutController::class, 'show']);
+Route::post('/checkout/{template}', [CheckoutController::class, 'proccess']);
